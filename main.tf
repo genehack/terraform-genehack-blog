769553bddf10101923eb1b9dfb22381fda50670b
John SJ Anderson <john@genehack.org>
2019-12-07 15:46:52 -0800

    r53-alias module config

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+// define providers
 provider "aws" {
   version = "~> 2.41"
   profile = "default"
@@ -8,14 +9,27 @@ provider "template" {
   version = "~> 2.1"
 }
 
+// FIXME need r53 zone
+
+// FIXME need IAM user for deployment
+
 module "site-main" {
   source           = "./site-main"
   region           = var.region
   domain           = var.domain
   site_bucket_name = "genehack.blog-site"
+  // FIXME set up logging bucket
   //  logs_bucket_name = "genehack.blog-logs"
   duplicate_content_penalty_secret = "FtgDeqHZgjbfGCH4zKgKEk4qxyYhE#"
   deployer                         = "genehack.blog-deployer"
   acm_certificate_arn              = "FIXME"
   not_found_response_path          = "error.html"
+}
+
+module "r53-alias" {
+  source             = "./r53-alias"
+  domain             = var.domain
+  target             = "${module.site-main.website_cdn_hostname}"
+  cdn_hosted_zone_id = "${module.site-main.website_cdn_zone_id}"
+  route53_zone_id    = "<r53-zone-id>"
 }
