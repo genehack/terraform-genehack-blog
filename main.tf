e23fd18d95f78a29ff7f3db123356f49f4071c8c
John SJ Anderson <john@genehack.org>
2019-12-07 15:45:13 -0800

    whitespace

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   version = "~> 2.41"
   profile = "default"
-  region = var.region
+  region  = var.region
 }
 
 provider "template" {
@@ -9,13 +9,13 @@ provider "template" {
 }
 
 module "site-main" {
-  source = "./site-main"
-  region = var.region
-  domain = "genehack.blog"
+  source           = "./site-main"
+  region           = var.region
+  domain           = var.domain
   site_bucket_name = "genehack.blog-site"
   //  logs_bucket_name = "genehack.blog-logs"
-  duplicate-content-penalty-secret = "FtgDeqHZgjbfGCH4zKgKEk4qxyYhE#"
-  deployer = "genehack.blog-deployer"
-  acm-certificate-arn = "fixme"
-  not-found-response-path = "error.html"  
+  duplicate_content_penalty_secret = "FtgDeqHZgjbfGCH4zKgKEk4qxyYhE#"
+  deployer                         = "genehack.blog-deployer"
+  acm_certificate_arn              = "FIXME"
+  not_found_response_path          = "error.html"
 }
--- site-main/outputs.tf
+++ site-main/outputs.tf
@@ -21,4 +21,3 @@ output "website_bucket_id" {
 output "website_bucket_arn" {
   value = aws_s3_bucket.website_bucket.arn
 }
-
