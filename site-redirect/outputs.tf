5eb91c9e5a8828b0abffbdafcfc74fb52daf9512
John SJ Anderson <john@genehack.org>
2019-12-07 10:11:34 -0800

    Remove the modules I'm not going to be using

--- site-redirect/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "website_cdn_hostname" {
-  value = aws_cloudfront_distribution.website_cdn.domain_name
-}
-
-output "website_cdn_zone_id" {
-  value = aws_cloudfront_distribution.website_cdn.hosted_zone_id
-}
-
