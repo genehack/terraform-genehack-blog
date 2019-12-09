830d008004bc3e2152f64fe0fad38f9d5bda94a9
John SJ Anderson <john@genehack.org>
2019-12-09 14:09:55 -0800

    whitespace

--- r53-zone/main.tf
+++ r53-zone/main.tf
@@ -6,8 +6,9 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_route53_zone" "main" {
-  name = var.domain
-  comment = var.comment
+  name         = var.domain
+  comment      = var.comment
+}
 
 resource "aws_route53_record" "cert_validation" {
   name    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
