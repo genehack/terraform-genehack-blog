460d38e1fee60cbfa4692ef2cd2fe32753276a0e
John SJ Anderson <john@genehack.org>
2019-12-09 18:27:56 -0800

    do all the cert shit in us-east-1

--- r53-zone/main.tf
+++ r53-zone/main.tf
@@ -1,13 +1,22 @@
 // Create Route53 zone
 
+// define a special provider for us-east-1 to force cert and cert verifier into that region
+provider "aws" {
+  version = "~> 2.41"
+  profile = "default"
+  region  = "us-east-1"
+  alias   = "us-east-1"
+}
+
 resource "aws_acm_certificate" "cert" {
+  provider          = aws.us-east-1
   domain_name       = var.domain
   validation_method = "DNS"
 }
 
 resource "aws_route53_zone" "main" {
-  name         = var.domain
-  comment      = var.comment
+  name    = var.domain
+  comment = var.comment
 }
 
 resource "aws_route53_record" "cert_validation" {
@@ -19,6 +28,7 @@ resource "aws_route53_record" "cert_validation" {
 }
 
 resource "aws_acm_certificate_validation" "cert" {
+  provider                = aws.us-east-1
   certificate_arn         = "${aws_acm_certificate.cert.arn}"
   validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
 }
